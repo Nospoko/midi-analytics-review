918e2117f3ebca69e9b08eb02ff8bbf2257f1f60
wmilkowska <wiktoria.milkowska@gmail.com>
2023-11-05 21:54:30 +0100

    add filtering steps

--- dashboard_pianoroll.py
+++ dashboard_pianoroll.py
@@ -32,58 +32,80 @@ def generated_piece_av(piece: MidiPiece, save_base: str) -> dict:
 
 def main():
     uploaded_file = st.file_uploader("Choose a file", type=["midi", "mid"])
-
     if uploaded_file is not None:
+        file_name = uploaded_file.name.replace(".mid", "")
+
         piece = MidiFile(uploaded_file).piece
         st.markdown("### Showing a pianoroll of the uploaded MIDI file")
         fig = ff.view.draw_pianoroll_with_velocities(piece)
         st.pyplot(fig, clear_figure=True)
 
-        generated_piece_av(piece, "uploaded_file")
-        st.audio("uploaded_file.mp3")
+        if not os.path.isdir(f"tmp/{file_name}"):
+            os.mkdir(f"tmp/{file_name}")
 
-        n_clustering = st.number_input(label="N parameter:", value=16)
+        original_paths = generated_piece_av(piece, f"tmp/{file_name}/{file_name}")
+        st.audio(original_paths["mp3"])
+
+        n_clustering = 16
         st.markdown("The N parameter specifies how many consecutive pitch values are used to distinguish identical fragments")
+
         df = clustering_process.run(piece, n=n_clustering)
         fragments = prepare_fragments(df=df, piece=piece, n=n_clustering)
+
+        filtering_method = st.selectbox(
+            label="Filtering method",
+            options=["Top 5", "Fastest", "Longest"],
+        )
 
         st.markdown("## Summary")
         n_fragments = len(fragments)
         st.markdown(f"Detected: {n_fragments} fragments")
 
-        for it, fragment in enumerate(fragments):
-            st.markdown(f"### Fragment {it}")
+        if filtering_method == "Top 5":
+            filtered_fragments = top_five_filtering(fragments)
+        elif filtering_method == "Fastest":
+            filtered_fragments = fastest_filtering(fragments)
+        elif filtering_method == "Longest":
+            filtered_fragments = longest_filtering(fragments)
+        else:
+            raise ValueError(f"Unknown filtering method: {filtering_method}")
 
-            variants = fragment["variants"]
-            n_variants = len(variants)
+        st.markdown("## Filtered Fragments")
+        for it, fragment in enumerate(filtered_fragments):
+            st.markdown(f"### Filtered Fragment {it}")
+            n_variants = len(fragment)
             st.markdown(f"This fragment has {n_variants} variants")
+            for it_v, variant in enumerate(fragment):
+                start = variant["start_note_index"]
+                finish = variant["finish_note_index"]
+                part_piece = piece[start:finish]
 
-            variant = variants[0]
-            start = variant["start_note_index"]
-            finish = variant["finish_note_index"]
-            part_piece = piece[start:finish]
+                st.markdown("Showing a pianoroll for one of the variants")
+                st.markdown(f"This variant has {part_piece.size} notes and {part_piece.duration:.2f} seconds")
 
-            st.markdown("Showing a pianoroll for one of the variants")
-            st.markdown(f"This variant has {part_piece.size} notes and {part_piece.duration:.2f} seconds")
+                fig = ff.view.draw_pianoroll_with_velocities(part_piece)
+                st.pyplot(fig, clear_figure=True)
 
-            fig = ff.view.draw_pianoroll_with_velocities(part_piece)
-            st.pyplot(fig, clear_figure=True)
-
-            generated_piece_av(part_piece, "part_piece")
-            st.audio("part_piece.mp3")
+                paths = generated_piece_av(part_piece, f"tmp/{file_name}/{filtering_method}-f{it}-v{it_v}")
+                st.audio(paths["mp3"])
 
 
-def prepare_fragments(df: pd.DataFrame, piece: MidiPiece, n: int) -> list[dict]:
-    # Filter fragments with most variants (top 5)
-    df = df.nlargest(5, "n_variants")
+def top_five_filtering(fragments: list[list[dict]]):
+    return fragments[:5]
 
+
+def fastest_filtering(fragments: list[list[dict]]):
+    return sorted(fragments, key=lambda x: sum(v["finish_time"] - v["start_time"] for v in x))[:5]
+
+
+def longest_filtering(fragments: list[list[dict]]):
+    return sorted(fragments, key=lambda x: sum(v["finish_time"] - v["start_time"] for v in x), reverse=True)[:5]
+
+
+def prepare_fragments(df: pd.DataFrame, piece: MidiPiece, n: int) -> list[list[dict]]:
     fragments = []
-    max_fragments = 5
-    max_variants = 5
-
     for it, row in df.iterrows():
         variants = []
-
         for idx in row.idxs:
             start_idx = idx - row.left_shift
             start_idx = max(start_idx, 0)
@@ -93,26 +115,14 @@ def prepare_fragments(df: pd.DataFrame, piece: MidiPiece, n: int) -> list[dict]:
             finish_idx = min(finish_idx, piece.size - 1)
             finish_time = piece.df.iloc[finish_idx].end
 
-            variant = dict(
-                start_time=start_time,
-                finish_time=finish_time,
-                start_note_index=int(start_idx),
-                finish_note_index=int(finish_idx),
-            )
+            variant = {
+                "start_time": start_time,
+                "finish_time": finish_time,
+                "start_note_index": start_idx,
+                "finish_note_index": finish_idx,
+            }
             variants.append(variant)
-
-            # Filter out fragments with long pauses
-            max_pause_duration = 2.0
-            if finish_time - start_time > max_pause_duration:
-                if len(variants) >= max_variants:
-                    break
-
-        if len(variants) > 0:
-            fragment = dict(variants=variants)
-            fragments.append(fragment)
-
-        if len(fragments) >= max_fragments:
-            break
+        fragments.append(variants)
 
     return fragments
 
